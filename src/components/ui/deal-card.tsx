965df37451b2c289fb3edf98816fc3f10f3e661f
Raul Alvarez <raulalgo@gmail.com>
2025-07-05 22:31:21 +0100

    Making progress in the deal card

--- src/components/ui/deal-card.tsx
+++ src/components/ui/deal-card.tsx
@@ -1,11 +1,24 @@
 import type * as React from "react";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardAction } from "@/components/ui/card";
+import { MapPin, EllipsisVertical } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { Button } from "./button";
 
 function DealCard({ className, ...props }: React.ComponentProps<"div">) {
   return (
-    <Card className={cn("flex flex-col gap-6 rounded-xl border border-card-border bg-card py-6 text-card-foreground shadow-sm", className)} {...props} />
+    <Card className="w-96 p-2">
+        <CardHeader className="p-0 flex items-center gap-2">
+            <MapPin className="size-5" />
+            <CardTitle className="p-0 grow">Deal Card</CardTitle>
+            <Button variant="ghost" size="icon" className="p-0">
+                <EllipsisVertical className="size-5" />
+            </Button>
+        </CardHeader>
+        <CardContent>
+            <p>This is a deal card</p>
+        </CardContent>
+    </Card>
   );
 }
 
