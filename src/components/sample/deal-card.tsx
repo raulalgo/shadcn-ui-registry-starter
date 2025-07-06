72abb2c43a3288b370d0baab065c2fe7ad1849d3
Raul Alvarez <raulalgo@gmail.com>
2025-07-06 22:26:07 +0100

    experimentando con el registry

---
+++ src/components/sample/deal-card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Chip } from "../ui/chip";
+import { MapPin } from "lucide-react";
+import { DealCard as DealCardComponent } from "../ui/deal-card";
+
+export const DealCard = () => {
+    return (
+        <DealCardComponent icon={<MapPin className="size-5" />} title="Location">
+            <Chip variant="include" label="Ciudad">Lisboa</Chip>
+            <Chip variant="exclude" >Porto</Chip>
+            <Chip variant="include" >Cascais</Chip>
+            <Chip variant="include" >Braga</Chip>
+            <Chip variant="include" >Aveiro</Chip>
+            <Chip variant="include" >Faro</Chip>
+            <Chip variant="include" >Carcabelos</Chip>
+        </DealCardComponent>
+    );
+};
