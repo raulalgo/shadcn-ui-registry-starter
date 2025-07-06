5cedd912e2ce79e797f069000e9f2310f4ea2dc6
Raul Alvarez <raulalgo@gmail.com>
2025-07-06 10:53:04 +0100

    Second example of deal card included

--- src/app/(design)/components/[slug]/(demos)/deal-card.tsx
+++ src/app/(design)/components/[slug]/(demos)/deal-card.tsx
@@ -1,11 +1,11 @@
 import { Chip } from "@/components/ui/chip";
 import { DealCard } from "@/components/ui/deal-card";
-import { MapPin } from "lucide-react";
+import { MapPin, Proportions } from "lucide-react";
 
 export const dealCard = {
   name: "deal-card",
   components: {
-    Default: <DealCard icon={<MapPin className="size-5" />} title="Location">
+    Location: <DealCard icon={<MapPin className="size-5" />} title="Location">
             <Chip variant="include" label="Ciudad">Lisboa</Chip>
             <Chip variant="exclude" >Porto</Chip>
             <Chip variant="include" >Cascais</Chip>
@@ -13,6 +13,13 @@ export const dealCard = {
             <Chip variant="include" >Aveiro</Chip>
             <Chip variant="include" >Faro</Chip>
             <Chip variant="include" >Carcabelos</Chip>
+        </DealCard>,
+    Format: <DealCard icon={<Proportions className="size-5" />} title="Format">
+            <Chip variant="include" >16:9</Chip>
+            <Chip variant="include" >Billboard</Chip>
+            <Chip variant="include" >Bus shelter</Chip>
+            <Chip variant="include" >Kiosk</Chip>
+            <Chip variant="exclude" >MUPI</Chip>
         </DealCard>,
   },
 };
