bc3445cfb237b361315598e0c3d7782e8ad8b347
Raul Alvarez <raulalgo@gmail.com>
2025-07-06 10:37:06 +0100

    Added the basic deal card architecture

--- src/components/ui/chip.tsx
+++ src/components/ui/chip.tsx
@@ -5,7 +5,7 @@ import type * as React from "react";
 import { cn } from "@/lib/utils";
 
 const chipVariants = cva(
-  "inline-flex items-center justify-center rounded-md border [&>#chip-content]:px-2 [&>#chip-content]:py-0.5 rounded-full [&>#chip-content]:rounded-full text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>span>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
+  "inline-flex items-center justify-center rounded-md border [&>#chip-content]:px-2 [&>#chip-content]:py-0.5 rounded-full [&>#chip-content]:rounded-full text-sm font-medium w-fit whitespace-nowrap shrink-0 [&>span>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden",
   {
     variants: {
       variant: {
--- src/components/ui/deal-card.tsx
+++ src/components/ui/deal-card.tsx
@@ -6,6 +6,15 @@ import { cn } from "@/lib/utils";
 import { Button } from "./button";
 import { Chip } from "./chip";
 
+function DealRow({className, children, ...props}: React.ComponentProps<"div">) {
+    return (
+        <div className={cn("w-full flex flex-row-reverse justify-between items-center gap-2 hover:[&>button]:block hover:bg-primary-50 min-h-8 rounded-sm", className)} {...props}>
+            {children}
+            <Button className="hidden" variant="ghost" size="sm">Add Objective</Button>
+        </div>
+    );
+}
+
 function DealCard({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <Card className="w-96 p-2 gap-0">
@@ -16,8 +25,11 @@ function DealCard({ className, ...props }: React.ComponentProps<"div">) {
                 <EllipsisVertical className="size-5" />
             </Button>
         </CardHeader>
-        <CardContent className="p-0">
-            <Chip variant="include">Lisboa</Chip>
+        <CardContent className="py-0 px-4">
+            <DealRow><Chip variant="include" label="Ciudad">Lisboa</Chip></DealRow>
+            <DealRow><Chip variant="exclude" >Porto</Chip></DealRow>
+            <DealRow><Chip variant="include" >Cascais</Chip></DealRow>
+            <DealRow><Chip variant="include" >Braga</Chip></DealRow>
         </CardContent>
     </Card>
   );
