0ee441f5c0ed08a7436509021f9ddd72bbbe6803
Raul Alvarez <raulalgo@gmail.com>
2025-07-07 23:06:44 +0100

    Cleanup to star with a minimal system

--- src/app/(design)/components/[slug]/(demos)/data-table.tsx
+++
@@ -1,8 +0,0 @@
-import { FinanceDataTable } from "@/app/(design)/components/[slug]/(demos)/data-table-payments";
-
-export const dataTable = {
-  name: "data-table",
-  components: {
-    Default: <FinanceDataTable />,
-  },
-};
