67ff522c4e6cbc40932a15d8e39074daf97470eb
Raul Alvarez <raulalgo@gmail.com>
2025-07-08 21:22:41 +0100

    Removed a couple of references more

--- src/app/(design)/components/[slug]/(demos)/badge.tsx
+++ src/app/(design)/components/[slug]/(demos)/badge.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { ArrowLeft, ArrowRight, Smile, CheckCircle2, XCircle } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 
 export const badge = {
