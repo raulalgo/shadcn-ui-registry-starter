485a01ab25c668efc151236fca959a557c21c2cb
Raul Alvarez <raulalgo@gmail.com>
2025-07-08 21:48:54 +0100

    Refactor de la card para simplificar el export

--- src/components/ui/deal-card.tsx
+++ src/components/ui/deal-card.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardAction } from "@/components/ui/card";
 import { EllipsisVertical } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -17,20 +16,20 @@ function DealRow({className, children, ...props}: React.ComponentProps<"div">) {
 
 function DealCard({ className, title, icon, children, ...props }: React.ComponentProps<"div"> & { title: string; icon?: React.ReactNode }) {
   return (
-    <Card className="w-96 p-2 gap-0">
-        <CardHeader className="p-0 flex items-center gap-2">
+    <div className="flex flex-col rounded-xl border border-card-border bg-card text-card-foreground shadow-sm w-96 p-2 gap-2">
+        <div className="flex flex-row gap-2 items-center">
             {icon}
-            <CardTitle className="p-0 grow">{title}</CardTitle>
+            <div className="font-semibold leading-none p-0 grow">{title}</div>
             <Button variant="ghost" size="icon" className="p-0">
                 <EllipsisVertical className="size-5" />
             </Button>
-        </CardHeader>
-        <CardContent className="py-0 px-4">
+        </div>
+        <div className="py-0 px-4">
             {React.Children.map(children, (child) => (
                 <DealRow>{child}</DealRow>
             ))}
-        </CardContent>
-    </Card>
+        </div>
+    </div>
   );
 }
 
