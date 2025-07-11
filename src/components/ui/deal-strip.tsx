3c4b64e1a6b3b6a90197d1c0938bcfda6d4006de
Raul Alvarez <raulalgo@gmail.com>
2025-07-11 21:59:14 +0100

    Adding dependencies to deal-strip registry

--- src/components/ui/deal-strip.tsx
+++ src/components/ui/deal-strip.tsx
@@ -1,9 +1,21 @@
 import type * as React from "react";
+import { DealCard } from "./deal-card";
+import { Badge } from "./badge";
+import { Calendar, LaptopMinimal } from "lucide-react";
 
 function DealStrip() {
   return (
-    <div>
-      Deal Strip
+    <div className="p-4 bg-neutral-200 gap-2 flex flex-col w-fit border-r border-neutral-900/10">
+      <DealCard title="Schedule" icon={<Calendar className="h-4 w-4" />}>
+        <Badge>15-08-2025 - 15-09-2025</Badge>
+      </DealCard>
+      <DealCard />
+      <DealCard title="Formats" icon={<LaptopMinimal className="h-4 w-4" />}>
+        <Badge label="Format">2m2</Badge>
+        <Badge>Billboard</Badge>
+        <Badge>16:9</Badge>
+        <Badge>Bus shelter</Badge>
+      </DealCard>
     </div>
   );
 }
