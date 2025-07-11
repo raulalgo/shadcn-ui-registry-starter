696658be693644d74307e1fa9393fa2f4ee23418
Raul Alvarez <raulalgo@gmail.com>
2025-07-11 15:31:22 +0100

    Refined the defaults of the deal card and added a second demo

--- src/app/(design)/components/[slug]/(demos)/deal-card.tsx
+++ src/app/(design)/components/[slug]/(demos)/deal-card.tsx
@@ -1,8 +1,16 @@
 import { DealCard } from "@/components/ui/deal-card";
+import { FileText } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 export const dealCard = {
   name: "deal-card",
   components: {
     Default: <DealCard />,
+    Formats: <DealCard title="Formats" icon={<FileText className="h-4 w-4" />}>
+        <Badge label="Format">2m2</Badge>
+        <Badge>Billboard</Badge>
+        <Badge>16:9</Badge>
+        <Badge>Bus shelter</Badge>
+      </DealCard>,
   },
 }; 
--- src/components/ui/deal-card.tsx
+++ src/components/ui/deal-card.tsx
@@ -1,4 +1,4 @@
-import type * as React from "react";
+import * as React from "react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "./button";
@@ -9,28 +9,34 @@ function DealRow({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full flex flex-row-reverse justify-between items-center gap-2 hover:[&>button]:block hover:bg-primary-50 min-h-8 rounded-sm [&>.objective-button]:hidden hover:[&>.objective-button]:block">
       {children}
-      <Button variant="medium" size="icon" className="objective-button" >Add Objective</Button>
+      <Button variant="medium" size="sm" className="objective-button" >Add Objective</Button>
     </div>
   );
 }
 
+const defaultChildren = [
+  <Badge label="City">Lisboa</Badge>,
+  <Badge>Port</Badge>,
+  <Badge>Aveiro</Badge>,
+  <Badge>Braga</Badge>,
+  <Badge>Faro</Badge>,
+  <Badge>Coimbra</Badge>,
+]
 
-function DealCard({ title, icon, children }: { title?: string, icon?: React.ReactNode, children?: React.ReactNode }) {
+
+function DealCard({ title = "Location", icon, children = defaultChildren }: { title?: string, icon?: React.ReactNode, children?: React.ReactNode }) {
   return (
     <div className="flex flex-col rounded-xl border border-card-border bg-card text-card-foreground shadow-sm w-96 p-2 gap-2">
         <div className="flex flex-row gap-2 items-center">
             {icon || <MapPin className="h-4 w-4" />}
-            <div className="font-semibold leading-none p-0 grow">{title || "Default Title"}</div>
-            <Button variant="ghost" size="icon" className="p-0"><EllipsisVertical /></Button>
+            <div className="font-semibold leading-none p-0 grow">{title}</div>
+            <Button variant="ghost" size="icon"><EllipsisVertical /></Button>
         </div>
         <div className="py-0 px-4">
-          {children || (
-            <>
-              <DealRow><Badge /></DealRow>
-              <DealRow><Badge /></DealRow>
-              <DealRow><Badge /></DealRow>
-            </>
-          )}
+          {React.Children.map(children, (child, index) => (
+            <DealRow key={index}>{child}</DealRow>
+          ))
+        }
         </div>
     </div>
   );
