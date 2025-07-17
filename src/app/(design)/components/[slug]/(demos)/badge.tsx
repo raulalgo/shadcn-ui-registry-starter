348d976663cc05cf0db639c23dd72ae8ad4137c0
Raul Alvarez <raulalgo@gmail.com>
2025-07-17 16:03:39 +0100

    Enhance Badge component with additional variants
    - Added new badge variants: primary, neutral, pinkred, sky, green, yellow, lime, orange, blue, pink, brown, and purple.
    - Updated badge.tsx to reflect the new variants and their respective labels.
    - Modified badge.tsx to improve content representation for each variant.

--- src/app/(design)/components/[slug]/(demos)/badge.tsx
+++ src/app/(design)/components/[slug]/(demos)/badge.tsx
@@ -3,8 +3,19 @@ import { Badge } from "@/components/ui/badge";
 export const badge = {
   name: "badge",
   components: {
-    start: <Badge variant="include" label="Ciudad">Porto</Badge>,
-    uno: <Badge variant="include">Aveiro</Badge>,
-    dos: <Badge variant="exclude">Lisboa</Badge>
+    include: <Badge variant="include" label="Include">Content</Badge>,
+    exclude: <Badge variant="exclude" label="Exclude">Content</Badge>,
+    primary: <Badge variant="primary" label="Primary">Content</Badge>,
+    neutral: <Badge variant="neutral" label="Neutral">Content</Badge>,
+    pinkred: <Badge variant="pinkred" label="PinkRed">Content</Badge>,
+    sky: <Badge variant="sky" label="Sky">Content</Badge>,
+    green: <Badge variant="green" label="Green">Content</Badge>,
+    yellow: <Badge variant="yellow" label="Yellow">Content</Badge>,
+    lime: <Badge variant="lime" label="Lime">Content</Badge>,
+    orange: <Badge variant="orange" label="Orange">Content</Badge>,
+    blue: <Badge variant="blue" label="Blue">Content</Badge>,
+    pink: <Badge variant="pink" label="Pink">Content</Badge>,
+    brown: <Badge variant="brown" label="Brown">Content</Badge>,
+    purple: <Badge variant="purple" label="Purple">Content</Badge>,
   },
 };
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -13,6 +13,30 @@ const badgeVariants = cva(
           "bg-primary-600/5 text-primary-700 [&>.chip-content]:bg-primary-700 [&>.chip-content]:text-primary-50",
         exclude:
           "bg-neutral-950/5 text-neutral-700 [&>.chip-content]:bg-neutral-700 [&>.chip-content]:text-neutral-50",
+        primary:
+          "bg-primary-600/5 text-primary-700 [&>.chip-content]:bg-primary-700 [&>.chip-content]:text-primary-50",
+        neutral:
+          "bg-neutral-600/5 text-neutral-700 [&>.chip-content]:bg-neutral-700 [&>.chip-content]:text-neutral-50",
+        pinkred:
+          "bg-pinkred-600/5 text-pinkred-700 [&>.chip-content]:bg-pinkred-700 [&>.chip-content]:text-pinkred-50",
+        sky:
+          "bg-sky-600/5 text-sky-700 [&>.chip-content]:bg-sky-700 [&>.chip-content]:text-sky-50",
+        green:
+          "bg-green-600/5 text-green-700 [&>.chip-content]:bg-green-700 [&>.chip-content]:text-green-50",
+        yellow:
+          "bg-yellow-600/5 text-yellow-700 [&>.chip-content]:bg-yellow-700 [&>.chip-content]:text-yellow-50",
+        lime:
+          "bg-lime-600/5 text-lime-700 [&>.chip-content]:bg-lime-700 [&>.chip-content]:text-lime-50",
+        orange:
+          "bg-orange-600/5 text-orange-700 [&>.chip-content]:bg-orange-700 [&>.chip-content]:text-orange-50",
+        blue:
+          "bg-blue-600/5 text-blue-700 [&>.chip-content]:bg-blue-700 [&>.chip-content]:text-blue-50",
+        pink:
+          "bg-pink-600/5 text-pink-700 [&>.chip-content]:bg-pink-700 [&>.chip-content]:text-pink-50",
+        brown:
+          "bg-brown-600/5 text-brown-700 [&>.chip-content]:bg-brown-700 [&>.chip-content]:text-brown-50",
+        purple:
+          "bg-purple-600/5 text-purple-700 [&>.chip-content]:bg-purple-700 [&>.chip-content]:text-purple-50",
       }
     }
   }
@@ -37,7 +61,7 @@ VariantProps<typeof badgeVariants> & { asChild?: boolean; label?: string }) {
       <span className="chip-content flex items-center justify-center">
       {children}
       </span>
-      </Comp>
+    </Comp>
   );
 }
 
