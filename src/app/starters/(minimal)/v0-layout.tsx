ebbdad997bd44dc2ee38b8f75d782fe03be925e1
Raul Alvarez <raulalgo@gmail.com>
2025-07-24 16:56:27 +0100

    Add globals.css to registry and update import order in v0-layout.tsx

--- src/app/starters/(minimal)/v0-layout.tsx
+++ src/app/starters/(minimal)/v0-layout.tsx
@@ -3,7 +3,8 @@ import React, { type ReactNode } from "react";
 
 import { cn } from "@/lib/utils";
 
-import '@/tailwind.css'
+import '@/app/globals.css'
+import '@/app/tailwind.css'
 
 const GeistSans = Geist({
   subsets: ["latin"],
