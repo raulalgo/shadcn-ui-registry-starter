3ba328311fdbbde1d953c44f717f5c610a55a268
Raul Alvarez <raulalgo@gmail.com>
2025-07-25 14:53:48 +0100

    Update MapView component to ensure full height for layout consistency

--- src/app/starters/(minimal)/map_view/page.tsx
+++ src/app/starters/(minimal)/map_view/page.tsx
@@ -9,7 +9,7 @@ export default async function MapView() {
       <main className="p-2 w-full h-full">
         <div className="flex flex-col bg-neutral-50 w-full h-full rounded-md overflow-hidden">
         <HeaderPlanner />
-        <div className="flex flex-row rounded-md shadow-md overflow-hidden w-full ">
+        <div className="flex flex-row rounded-md shadow-md overflow-hidden w-full h-full">
           <DealStrip className="w-96 h-full overflow-y-auto" />
           <div className="flex-1 h-full w-full bg-neutral-100">
             {/* Content here */}
