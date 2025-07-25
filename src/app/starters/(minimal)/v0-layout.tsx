3b022762821a857434a053664b87487e8d27d43c
Raul Alvarez <raulalgo@gmail.com>
2025-07-25 22:05:30 +0100

    Update import in v0-layout.tsx from tailwind.css to globals.css to reflect recent style refactor and maintain consistency in project structure.

--- src/app/starters/(minimal)/v0-layout.tsx
+++ src/app/starters/(minimal)/v0-layout.tsx
@@ -3,7 +3,7 @@ import React, { type ReactNode } from "react";
 
 import { cn } from "@/lib/utils";
 
-import '@/tailwind.css'
+import '@/globals.css'
 
 const GeistSans = Geist({
   subsets: ["latin"],
