2ab84e60759cad5f1ed085589f393e05c3e7cc6d
Raul Alvarez <raulalgo@gmail.com>
2025-09-16 10:18:36 +0100

    Refactor List View and Remove New List Component
    - Updated the List View component to use DataTableAGGridDemo for displaying employee management data.
    - Changed the description in registry.json for the List View block.
    - Removed the New List component and its associated files from the project.

--- src/app/starters/(minimal)/list_view/page.tsx
+++ src/app/starters/(minimal)/list_view/page.tsx
@@ -1,27 +1,20 @@
 import { GlobalNav } from "@/components/ui/global-nav";
-import { Input } from "@/components/ui/input";
 import { L1Header } from "@/components/ui/l1-header";
 import { Button } from "@/components/ui/button";
-import { FilterIcon, PlusIcon, SearchIcon } from "lucide-react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { DataTableDemo } from "@/components/ui/data-table";
+import { DataTableAGGridDemo } from "@/components/ui/data-table-ag-grid-demo";
+import { Toaster } from "@/components/ui/sonner";
 
-export default async function MapView() {
+export default async function ListView() {
   return (
     <div className="bg-primary-900 flex h-screen w-screen flex-row py-2 pr-2">
       <GlobalNav className="h-full" selectedPage="campaigns" />
-      <main className="h-full w-full overflow-y-scroll rounded-md bg-neutral-100">
+      <main className="flex h-full w-full flex-col overflow-y-scroll rounded-md bg-neutral-100">
         <L1Header h1="Campaign Management" />
-        <div className="p-4">
-          <DataTableDemo />
+        <div className="flex-1 px-4">
+          <DataTableAGGridDemo />
         </div>
       </main>
+      <Toaster />
     </div>
   );
 }
